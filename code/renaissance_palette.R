ff1b7f8d81ad4e03b01477f86f276281f25311d5
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-05-03 19:35:46 +0200

    added triples gathering

--- code/renaissance_palette.R
+++ code/renaissance_palette.R
@@ -1,8 +1,13 @@
 library(pixmap)
+library(dplyr)
 #convert aminadab_rgb.jpg aminadab_rgb.ppm
 read.pnm("images/aminadab_rgb.ppm")
 painting_michelangelo <- read.pnm("images/aminadab_rgb.ppm")
-dim(painting_michelangelo)
 str(painting_michelangelo)
 plot(painting_michelangelo)
-plot(painting_michelangelo[1,1])
+red_vector           <- as.vector(painting_michelangelo@red)
+green_vector         <- as.vector(painting_michelangelo@green)
+blue_vector          <- as.vector(painting_michelangelo@blue)
+
+data.frame(red_vector,green_vector,blue_vector) %>% 
+  unique() -> rgb_triples
