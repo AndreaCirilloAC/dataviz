2103b5993dc27f5342cf823768814c7c4bac28a4
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-05-04 22:57:37 +0200

    alternative way to obtain palette:clustering

---
+++ code/renaissance_palette_clustering.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(jpeg)
+library(ggplot2)
+#read file
+#thanks to https://alstatr.blogspot.it/2014/09/r-k-means-clustering-on-image.html
+painting <- readJPEG("images/profeta_daniele.jpg")
+dimension <- dim(painting)
+painting_rgb <- data.frame(
+  x = rep(1:dimension[2], each = dimension[1]),
+  y = rep(dimension[1]:1, dimension[2]),
+  R = as.vector(painting[,,1]),
+  G = as.vector(painting[,,2]),
+  B = as.vector(painting[,,3])
+)
+
+#apply clustering
+
+n_clusters <- 20
+k_means    <- kmeans(painting_rgb[,c("R","G","B")], centers = n_clusters, iter.max = 30)
+colours_k  <- rgb(k_means$centers[k_means$cluster,])
+colours_vector <- unique(colours_k)
+show_col(colours_vector)
+
+ggplot(data = painting_rgb, aes(x = x, y = y)) + 
+  geom_point(colour = colours_k)
