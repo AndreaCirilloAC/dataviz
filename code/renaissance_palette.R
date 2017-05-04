616e56159d44bd75474c845d55d0d1566e2b93cd
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-05-04 07:11:12 +0200

    wrapped code in a function to sample palette

--- code/renaissance_palette.R
+++ code/renaissance_palette.R
@@ -1,27 +1,19 @@
 library(pixmap)
 library(dplyr)
+library(scales)
 #convert aminadab_rgb.jpg aminadab_rgb.ppm
 
-painting_michelangelo <- read.pnm("images/aminadab_rgb.ppm")
-str(painting_michelangelo)
-plot(painting_michelangelo)
-red_vector           <- as.vector(painting_michelangelo@red)
-green_vector         <- as.vector(painting_michelangelo@green)
-blue_vector          <- as.vector(painting_michelangelo@blue)
+palette_sampler <- function(path_to_pnm,n_sample){
+  pnm <- read.pnm(path_to_pnm)
+  red_vector           <- as.vector(pnm@red)
+  green_vector         <- as.vector(pnm@green)
+  blue_vector          <- as.vector(pnm@blue)
+  
+  data.frame(red_vector,green_vector,blue_vector) %>% 
+    unique() -> rgb_triples
+  rgb_codes <- rgb(red = rgb_triples[,1],green = rgb_triples[,2], blue = rgb_triples[,3])
+  return(sample(rgb_codes,n_sample))
+}
 
-data.frame(red_vector,green_vector,blue_vector) %>% 
-  unique() -> rgb_triples
-rgb_codes <- rgb(red = rgb_triples[,1],green = rgb_triples[,2], blue = rgb_triples[,3])
-
-show_col(rgb_codes)
-
-painting_raffaello <- read.pnm("images/sacra_famiglia_canigiani.ppm")
-red_vector           <- as.vector(painting_raffaello@red)
-green_vector         <- as.vector(painting_raffaello@green)
-blue_vector          <- as.vector(painting_raffaello@blue)
-
-data.frame(red_vector,green_vector,blue_vector) %>% 
-  unique() -> rgb_triples
-rgb_codes <- rgb(red = rgb_triples[,1],green = rgb_triples[,2], blue = rgb_triples[,3])
-
-show_col(rgb_codes)
+sample_400 <- palette_sampler("images/sacra_famiglia_canigiani.ppm",400)
+show_col(sample_400)
