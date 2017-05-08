69782b948afe6b7effd186c7a99bac47b5ae86c2
Andrea Cirillo <andreacirilloac@gmail.com>
2017-05-08 21:43:56 +0200

    polished things a bit up

--- code/renaissance_palette_clustering.R
+++ code/renaissance_palette_clustering.R
@@ -2,8 +2,8 @@ library(dplyr)
 library(jpeg)
 library(ggplot2)
 #read file
-painting <- readJPEG("images/profeta_daniele.jpg")
-dimension <- dim(painting)
+painting     <- readJPEG("images/profeta_daniele.jpg")
+dimension    <- dim(painting)
 painting_rgb <- data.frame(
   x = rep(1:dimension[2], each = dimension[1]),
   y = rep(dimension[1]:1, dimension[2]),
@@ -14,9 +14,9 @@ painting_rgb <- data.frame(
 
 #apply clustering
 
-n_clusters <- 20
-k_means    <- kmeans(painting_rgb[,c("R","G","B")], centers = n_clusters, iter.max = 30)
-colours_k  <- rgb(k_means$centers[k_means$cluster,])
+n_clusters     <- 20
+k_means        <- kmeans(painting_rgb[,c("R","G","B")], centers = n_clusters, iter.max = 30)
+colours_k      <- rgb(k_means$centers[k_means$cluster,])
 colours_vector <- unique(colours_k)
 show_col(colours_vector)
 
